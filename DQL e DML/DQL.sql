1c4296118088b94d2ac6d952f8f3ea8f84b75d97
Guilherme Melo <112731659+GuiMelo012x@users.noreply.github.com>
2024-11-03 16:00:00 -0300

    Update DQL.sql

--- DQL e DML/DQL.sql
+++ DQL e DML/DQL.sql
@@ -1,68 +1,54 @@
 -- Pergunta 1
-SELECT COUNT(*) AS Total_Inscricoes FROM Inscricao;
-
+SELECT SUM(Inscricao.Valor) AS TotalArrecadado
+FROM Inscricao
+WHERE Inscricao.Status = 'Realizado';
 -- Pergunta 2
-SELECT P.Nome AS Nome_Medico, COUNT(C.ID_Consulta) AS Total_Consultas
-     FROM Medico M
-     JOIN Pessoa P ON M.ID_Pessoa = P.ID_Pessoa
-     LEFT JOIN Consulta C ON M.ID_Medico = C.ID_Medico 
-     GROUP BY P.Nome;
-
-
-
+SELECT Medico.ID_Medico, Pessoa.Nome, COUNT(Consulta.ID_Consulta) AS TotalConsultas
+FROM Consulta
+JOIN Medico ON Consulta.ID_Medico = Medico.ID_Medico
+JOIN Pessoa ON Medico.ID_Pessoa = Pessoa.ID_Pessoa
+GROUP BY Medico.ID_Medico, Pessoa.Nome
+ORDER BY TotalConsultas DESC;
 -- Pergunta 3
-SELECT Hora, COUNT(*) AS Total_Pedidos
+SELECT Pedido.Hora, COUNT(Pedido.ID_Pedido) AS TotalPedidos
 FROM Pedido
-GROUP BY Hora
-ORDER BY Total_Pedidos DESC:
-
-
-
+GROUP BY Pedido.Hora
+ORDER BY TotalPedidos DESC
+LIMIT 1;
 -- Pergunta 4
-SELECT P.Nome, R.Turno
-FROM Funcionario F
-JOIN Pessoa P ON F.ID_Pessoa = P.ID_Pessoa
-JOIN Recepcao R ON F.ID_Funcionario = R.ID_Funcionario;
-
+SELECT Funcionario.ID_Funcionario, Pessoa.Nome, Recepcao.Turno
+FROM Recepcao
+JOIN Funcionario ON Recepcao.ID_Funcionario = Funcionario.ID_Funcionario
+JOIN Pessoa ON Funcionario.ID_Pessoa = Pessoa.ID_Pessoa;
 -- Pergunta 5
-SELECT Status, COUNT(*) AS Total_Mesas
+SELECT Mesa.Status, COUNT(Mesa.ID_Mesa) AS QuantidadeMesas
 FROM Mesa
-GROUP BY Status;
-
+WHERE Mesa.Status IN ('Ocupada', 'Reservada')
+GROUP BY Mesa.Status;
 -- Pergunta 6
-SELECT COUNT(DISTINCT P.ID_Paciente) AS Total_Pacientes, M.Especialidade, COUNT(*) AS Total_Consultas
-FROM Paciente P
-JOIN Consulta C ON P.ID_Paciente = C.ID_Paciente
-JOIN Medico M ON C.ID_Medico = M.ID_Medico
-GROUP BY M.Especialidade;
-
-
--- Pergunta 7 ESTÁ DANDO PROBLEMA
+SELECT COUNT(DISTINCT Consulta.ID_Paciente) AS TotalPacientes, Medico.Especializacao, COUNT(Consulta.ID_Consulta) AS TotalConsultas
+FROM Consulta
+JOIN Medico ON Consulta.ID_Medico = Medico.ID_Medico
+GROUP BY Medico.Especializacao
+ORDER BY TotalConsultas DESC;
+-- Pergunta 7
 SELECT Lugar.ID_Lugar, Lugar.Nome, Lugar.Capacidade, COUNT(DISTINCT Participante.ID_Participante) AS TotalParticipantes
 FROM Lugar
 JOIN Evento ON Lugar.ID_Lugar = Evento.ID_Lugar
 JOIN Participante ON Evento.ID_Evento = Participante.ID_Evento
 GROUP BY Lugar.ID_Lugar, Lugar.Nome, Lugar.Capacidade;
-
-
 -- Pergunta 8
-SELECT I.Ingredientes, COUNT(*) AS Frequencia
-FROM Itens I
-JOIN Pedido P ON I.ID_Pedido = P.ID_Pedido
-GROUP BY I.Ingredientes
-ORDER BY Frequencia DESC
-LIMIT 5;  -- Para os 5 mais frequentes
-
--- Pergunta 9
--- total de itens:
-SELECT SUM(Quantidade) AS Total_Itens_Servidos FROM Itens;
-#ingredientes utilizados
-SELECT Ingredientes, SUM(Quantidade) AS Total_Quantidade
+SELECT Itens.ID_Item, COUNT(Itens.ID_Pedido) AS FrequenciaConsumo
 FROM Itens
-GROUP BY Ingredientes
-ORDER BY Total_Quantidade DESC;
-
+GROUP BY Itens.ID_Item
+ORDER BY FrequenciaConsumo DESC;
+-- Pergunta 9
+SELECT SUM(Itens.Quantidade) AS TotalItensServidos, Itens.Ingredientes, COUNT(Itens.ID_Item) AS FrequenciaIngrediente
+FROM Itens
+GROUP BY Itens.Ingredientes
+ORDER BY FrequenciaIngrediente DESC;
 -- Pergunta 10
--- total
-SELECT COUNT(*) AS Total_Inscricoes FROM Inscricao;
- -- tipos está com erro
+SELECT Evento.tipo_evento, COUNT(Inscricao.ID_Inscricao) AS TotalInscricoes
+FROM Inscricao
+JOIN Evento ON Inscricao.ID_Evento = Evento.ID_Evento
+GROUP BY Evento.tipo_evento;
